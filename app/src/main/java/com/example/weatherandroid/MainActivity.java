86697b419de14cd0eef435f5e0f43c466070bdcb
AlexKherson <q0992358472@gmail.com>
2020-11-27 21:54:04 +0200

    add menu

--- app/src/main/java/com/example/weatherandroid/MainActivity.java
+++ app/src/main/java/com/example/weatherandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.weatherandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_setting:
+                break;
+            case R.id.menu_location:
+                break;
+            case R.id.menu_style:
+                break;
+            case R.id.menu_info:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
